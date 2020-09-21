90bb0da0b79e3ffd8a12b0ccd3601097fd448169
J. Christian Romero <jcromerohdz@gmail.com>
2020-09-21 08:04:38 -0700

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Lorem ipsum dolor sit amet consectetur adipiscing elit at, non viverra senectus integer suspendisse sapien tincidunt arcu ad, lacinia id luctus primis libero rutrum tristique."
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My greate service",
+    body: "Lorem ipsum dolor sit amet consectetur adipiscing elit at, non viverra senectus integer suspendisse sapien tincidunt arcu ad, lacinia id luctus primis libero rutrum tristique.",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200" ) 
+end 
+
+puts "9 portfolio created"
